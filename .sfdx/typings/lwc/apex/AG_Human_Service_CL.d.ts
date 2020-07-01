a21cfeae6df1a4decdf4e3a359799f4424cf99e6
prabu nagul <prabu@aspigrow.com>
2020-07-01 20:20:04 +0530

    Added service component

--- .sfdx/typings/lwc/apex/AG_Human_Service_CL.d.ts
+++ .sfdx/typings/lwc/apex/AG_Human_Service_CL.d.ts
@@ -1,6 +1,18 @@
 declare module "@salesforce/apex/AG_Human_Service_CL.changeStatus" {
   export default function changeStatus(param: {recordId: any, status: any}): Promise<any>;
 }
+declare module "@salesforce/apex/AG_Human_Service_CL.createSurveyInvitationLink" {
+  export default function createSurveyInvitationLink(param: {recordId: any}): Promise<any>;
+}
 declare module "@salesforce/apex/AG_Human_Service_CL.changeStatusForLead" {
   export default function changeStatusForLead(param: {recordId: any, status: any}): Promise<any>;
+}
+declare module "@salesforce/apex/AG_Human_Service_CL.getServiceRequest" {
+  export default function getServiceRequest(param: {intakeId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/AG_Human_Service_CL.getServicesForServiceType" {
+  export default function getServicesForServiceType(param: {searchKey: any, serviceType: any}): Promise<any>;
+}
+declare module "@salesforce/apex/AG_Human_Service_CL.saveServiceLineItem" {
+  export default function saveServiceLineItem(param: {intakeId: any, serviceId: any, fromDate: any, toDate: any}): Promise<any>;
 }
