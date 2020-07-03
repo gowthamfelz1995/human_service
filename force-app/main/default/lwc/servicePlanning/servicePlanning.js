66b2b199a56c0e132087ca4eb96984117e168e0d
prabu nagul <prabu@aspigrow.com>
2020-07-03 08:57:07 +0530

    Added changes

--- force-app/main/default/lwc/servicePlanning/servicePlanning.js
+++ force-app/main/default/lwc/servicePlanning/servicePlanning.js
@@ -15,64 +15,104 @@ import SERVICE_REQUEST_OBJECT from '@salesforce/schema/AG_Service_Request__c';
 
 import getServiceRequest from '@salesforce/apex/AG_Human_Service_CL.getServiceRequest';
 
-import getServicesForServiceType from '@salesforce/apex/AG_Human_Service_CL.getServicesForServiceType';
+import saveServiceLineItem from '@salesforce/apex/AG_Human_Service_CL.saveServiceLineItem';
 
-const DELAY = 350;
 export default class ServicePlanning extends NavigationMixin(LightningElement) {
 
     @api recordId;
 
+    @api intake;
+
     @api serviceRequest = SERVICE_REQUEST_OBJECT;
-
-    @api serviceRequestId;
-
-    @api serviceRequestName;
 
     @api serviceType;
 
     @api services;
 
+    @api serviceLineItemId;
+
+    @api checkedValue;
+
+    @api fromDate;
+
+    @api toDate;
+
+    @api filteredServices;
+
     connectedCallback() {
         getServiceRequest({
-                intakeId: this.recordId
+                recordId: this.recordId
             })
             .then((result) => {
                 const response = JSON.parse(result);
-                this.serviceRequestId = response.data.Id
-                this.serviceRequestName = response.data.Name;
-                this.serviceType = response.data.AG_Service_Type__c;
+                this.intake = response.serviceRequest.AG_Intake__c;
+                this.serviceType = response.serviceRequest.AG_Service_Type__c;
+                this.services = response.serviceWrapperList;
             })
             .catch((error) => {
-                this.error = error;
+                this.error = response.message;
             })
     }
 
-    handleService(event) {
-        window.clearTimeout(this.delayTimeout);
-        const searchKey = event.target.value;
-        this.delayTimeout = setTimeout(() => {
-            getServicesForServiceType({
-                    searchKey,
-                    serviceType: this.serviceType
-                })
-                .then((result) => {
-                    const response = JSON.parse(result);
-                    this.services = response.data;
-                    this.error = undefined;
-                })
-                .catch((error) => {
-                    this.error = error;
-                    this.contacts = undefined;
-                });
-        }, DELAY);
+    getFromDate(event) {
+        this.fromDate = event.target.value;
     }
 
-    handleCancel(){
+    getToDate(event) {
+        this.toDate = event.target.value;
+    }
+
+    handleChange(event) {
+        this.checkedValue = event.target.checked;
+        console.log('checkedValue--->' + this.checkedValue);
+        for (var service of this.services) {
+            console.log('service------->' + JSON.stringify(service));
+            var checkedService = this.services[this.checkedValue];
+            console.log('checkedService------->' +checkedService);
+
+        }
+    }
+
+    handleSave(event) {
+        saveServiceLineItem({
+                intakeId: this.intake,
+                services: this.filteredServices,
+                fromDate: this.fromDate,
+                toDate: this.toDate
+            })
+            .then((result) => {
+                const response = JSON.parse(result);
+                this.serviceLineItemId = response.data.Id;
+                const successEvent = new ShowToastEvent({
+                    title: "Success",
+                    message: response.message,
+                    variant: "success"
+                });
+                this.dispatchEvent(successEvent);
+            })
+            .catch((error) => {
+                this.error = response.message;
+            })
+        this.returnToServiceLineItem();
+    }
+
+    handleCancel() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
                 recordId: this.recordId,
-                objectApiName: 'AG_Intake__c',
+                objectApiName: 'AG_Service_Request__c',
+                actionName: 'view'
+            }
+        });
+    }
+
+    returnToServiceLineItem() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.serviceLineItemId,
+                objectApiName: 'AG_Service_Line_Item__c',
                 actionName: 'view'
             }
         });
