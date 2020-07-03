e8f0859bb7a092481def07728ab4eefc272429b9
gowthamaspi <gowtham@aspigrow.com>
2020-07-03 08:54:10 +0530

    Added changes in assessment

--- force-app/main/default/lwc/assessment/assessment.js
+++ force-app/main/default/lwc/assessment/assessment.js
@@ -14,12 +14,11 @@ export default class Assessment extends LightningElement {
     handleKeyChange(event) {
         // Debouncing this method: Do not update the reactive property as long as this function is
         // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-        window.clearTimeout(this.delayTimeout);
         const searchKey = event.target.value;
         // eslint-disable-next-line @lwc/lwc/no-async-operation
-        this.delayTimeout = setTimeout(() => {
+        
             this.searchKey = searchKey;
-        }, DELAY);
+       console.log(searchKey)
     }
     getAssessmentKey(event){
     console.log(event.target.key)
