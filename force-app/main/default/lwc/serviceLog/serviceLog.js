d1faa22855ec7b06cb9b7cd344a0745cb55f544f
gowthamaspi <gowtham@aspigrow.com>
2020-07-04 10:43:35 +0530

    Removed search layout

--- force-app/main/default/lwc/serviceLog/serviceLog.js
+++ force-app/main/default/lwc/serviceLog/serviceLog.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api,track } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import {
+    ShowToastEvent
+  } from 'lightning/platformShowToastEvent';
 
-export default class ServiceLog extends LightningElement {} 
+export default class ServiceLog extends LightningElement {
+    @track questionList = [];
+} 
